0b51d9e573b1aaa16677bcffb1388fa476ebe7ef
mohannadd mohammad <113944930+mohandalaidy171@users.noreply.github.com>
2025-07-29 20:51:24 +0300

    Update mocha.spec.js

--- cypress/integration/mocha.spec.js
+++ cypress/integration/mocha.spec.js
@@ -21,14 +21,19 @@ describe('commitquality Test Suite', () => {
     cy.clearCookies();
     cy.clearLocalStorage();
   });
-  it('should login with valid data and add new product', () => {
 
-    cy.get('a[data-testid="navbar-login"]', { timeout: 10000 }).click();
+  it('should login with valid data and add new product', () => {
+cy.get('a[data-testid="navbar-login"]', { timeout: 10000 }).click();
     cy.get('input[data-testid="username-textbox"]', { timeout: 10000 }).type('test');
     cy.get('input[data-testid="password-textbox"]', { timeout: 10000 }).type('test');
     cy.get('button[data-testid="login-button"]', { timeout: 10000 }).click();
 
     cy.get('a[data-testid="navbar-logout"]', { timeout: 10000 }).should('have.text', 'Logout');
+
+  });
+  it('verify add new product', () => {
+
+    
 let productName='mohannad new product';
     cy.get('a[data-testid="add-a-product-button"]', { timeout: 10000 }).click();
     cy.get('input[data-testid="product-textbox"]', { timeout: 10000 }).type(productName);
@@ -42,10 +47,14 @@ let productName='mohannad new product';
 cy.get('td[data-testid="name"]', { timeout: 10000 }).should('have.text',productName);
 
 
-
-
   });
+it('verify reset functionality working correctly', () => {
+ cy.get('input[class="filter-textbox"]')
+  
 
+
+
+});
   it('should edit with price', () => {
   let price = 200;
   let productName = 'mohannad new product';
